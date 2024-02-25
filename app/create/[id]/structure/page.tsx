6a445a28566713e0eda5e3f3d786f7f6bc2b6fa5
Tianhao Zhang <zhangth1990@hotmail.com>
2024-02-25 13:58:56 -0700

    nested route

---
+++ app/create/[id]/structure/page.tsx
@@ -0,0 +1,9 @@
+export default function StructureRoute() {
+    return (
+        <>
+            <div className="w-3/5 mx-auto">
+                <h2>Which of these best describe your Home?</h2>
+            </div>
+        </>
+    );
+}
