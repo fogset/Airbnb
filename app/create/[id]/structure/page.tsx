8ffed280ec151a946e25e3d5415c74622ab53363
Tianhao Zhang <zhangth1990@hotmail.com>
2024-02-25 14:31:01 -0700

    added selectedCategroy and page

--- app/create/[id]/structure/page.tsx
+++ app/create/[id]/structure/page.tsx
@@ -1,9 +1,26 @@
+import { SelectCategory } from "@/app/components/SelectedCategrory";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function StructureRoute() {
     return (
         <>
             <div className="w-3/5 mx-auto">
-                <h2>Which of these best describe your Home?</h2>
+                <h2 className="text-3xl font-semibold tracking-tight transition-colors">
+                    Which of these best describe your Home?
+                </h2>
             </div>
+
+            <form>
+                <SelectCategory />
+                <div className="fixed w-full bottom-0 z-10 bg-white border-t h-24">
+                    <div className="flex items-center justify-between mx-auto px-5 lg:px-10 h-full">
+                        <Button variant="secondary" size="lg" asChild>
+                            <Link href="/">Cancel</Link>
+                        </Button>
+                        <Button size="lg">Save</Button>
+                    </div>
+                </div>
+            </form>
         </>
     );
 }
